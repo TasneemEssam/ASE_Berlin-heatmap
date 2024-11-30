6cc3fc80a556fc221d403bb58884f01706c1ce75
Tasneem Essam <tasneemessamshaheen@gmail.com>
2024-12-01 01:42:05 +0200

    Updated files and cleaned up repository

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 
-currentWorkingDirectory = "/home/tasneem/Workspace/Advance Software Engineering/Project 1/berlingeoheatmap_project1-main"
-#currentWorkingDirectory = "/mount/src/berlingeoheatmap1/"
-
 # -----------------------------------------------------------------------------
 import os
-os.chdir(currentWorkingDirectory)
-print("Current working directory\n" + os.getcwd())
 
 import pandas                        as pd
+import streamlit                     as st 
 from core import methods             as m1
 from core import HelperTools         as ht
 
@@ -22,6 +18,7 @@ def main():
     
     df_lstat        = pd.read_excel('datasets/Ladesaeulenregister_SEP.xlsx', header=10)
     df_residents    = pd.read_csv('datasets/plz_einwohner.csv')
+    
 
     df_lstat2       = m1.preprop_lstat(df_lstat, df_geodat_plz, pdict)
     gdf_lstat3      = m1.count_plz_occurrences(df_lstat2)
